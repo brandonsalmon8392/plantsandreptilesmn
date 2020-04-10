15f6023c5b92ab089e1404d03bc8e4b8859f3fce
tv8392uu <tv8392uu@go.minnstate.edu>
2020-04-09 22:30:38 -0500

    Added Carousel to about page

--- src/components/UI/Carousel/Carousel.js
+++ src/components/UI/Carousel/Carousel.js
@@ -1,38 +1,41 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
+import Carousel from 'react-bootstrap/Carousel'
 import alex from "../../../resources/images/alex.png"
 import lizard from "../../../resources/images/lizard.jpg"
 import brownsnake from "../../../resources/images/brownsnake2.jpg"
 import leopardfrog from "../../../resources/images/leopardfrog.jpg"
 import paintedturtle2 from "../../../resources/images/painted1.jpg"
 import racerunner from "../../../resources/images/racerunner.jpg"
-export default () => (
-    <Carousel autoPlay>
-        <div>
-            <img src={alex} />
-            <p className="legend">Painted Turtle</p>
-        </div>
-        <div>
-            <img src={lizard} />
-            <p className="legend">Common Five-lined Skink</p>
-        </div>
-        <div>
-            <img src={brownsnake} />
-            <p className="legend">Brownsnake</p>
-        </div>
-        <div>
-            <img src={leopardfrog} />
-            <p className="legend">Leopard Frog</p>
-        </div>
-        <div>
-            <img src={paintedturtle2} />
-            <p className="legend">Painted Turtle</p>
-        </div>
-        <div>
-        <img src={racerunner} />
-        <p className="legend">Six-lined Racerunner</p>
-    </div>
+import styles from "./Carousel.css"
+//asdasdasdasd
 
-
+const ReptileCarousel = () => (
+    <Carousel>
+        <Carousel.Item>
+            <img src={alex}/>
+            <Carousel.Caption><p>Painted Turtle</p></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img src={lizard}/>
+            <Carousel.Caption><p>Common Five-lined Skink</p></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img src={brownsnake}/>
+            <Carousel.Caption><p>Brownsnake</p></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img src={leopardfrog}/>
+            <Carousel.Caption><p>Leopard Frog</p></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img src={paintedturtle2}/>
+            <Carousel.Caption><p>Painted Turtle</p></Carousel.Caption>
+        </Carousel.Item>
+        < Carousel.Item>
+            < img src={racerunner}/>
+            <Carousel.Caption><p>Six-lined Racerunner</p></Carousel.Caption>
+        </Carousel.Item>
     </Carousel>
 );
+
+export default ReptileCarousel;
--- src/components/aboutPage/aboutPage.js
+++ src/components/aboutPage/aboutPage.js
@@ -1,15 +1,14 @@
 import React, {Component} from "react";
 import Carousel from "../UI/Carousel/Carousel";
 class aboutPage extends Component{
-
-//sdss
+    //asdasdasd
     render() {
         return (
             <div className="App-header position-absolute w-100">
                 <h1 style={{margin: "0 auto", color: "#74B266", marginBottom: "34px"}}>Help Preserve the Wildlife of Minnesota</h1>
                 <Carousel/>
                 <div>
-                    <h3 style={{color: "#74B266"}}>Conservation Efforts</h3>
+                    <h3 style={{color: "#74B266", marginTop: "30px"}}>Conservation Efforts</h3>
 
                     <a href="https://mnherpsoc.org/mn-herps/">Get involved with the Minnesota Herpetological Society </a><br/>
                     <a href="https://www.dnr.state.mn.us/nr/index.html">Learn more about Minnesota regulations</a><br/>
