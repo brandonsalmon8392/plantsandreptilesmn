6cde20a0a849a8133d68ed65e210ce284abfcd14
brandonsalmon <bsalmon@kettleriverconsulting.com>
2020-11-29 23:38:59 -0600

    Added Modal pop-up to cards to help enforce size limits

--- src/components/UI/InfoCard/InfoCard.js
+++ src/components/UI/InfoCard/InfoCard.js
@@ -7,60 +7,66 @@ import {useSpring, animated} from 'react-spring'
 const InfoCard = (props) => {
     const [displayModal, setDisplayModal] = useState(false);
 
-    const showAsModal = () => {
+    const toggleModal = () => {
         setDisplayModal(!displayModal);
     };
+
     const prop = useSpring({opacity: 1, from: {opacity: 0}});
     return (
         <animated.div style={prop} className="card-layout">
-            {displayModal ? <Modal show={displayModal} onHide={showAsModal}>
+            {displayModal ?
+                <Modal show={displayModal} onHide={toggleModal}>
+                        <div className="modal-body card-modal" style={{backgroundColor: "#282c34"}}>
+                            <img className="modalImage" src={props.data.image} placeholder="Image"/>
 
-                    <div className="card-modal modal-body">
-                        <img className="cardImage" src={props.data.image} placeholder="Image"/>
-                        <div className="card-info">
-                            <div>
-                                <h4>{props.data.species}</h4>
-                                <p style={{height: "20%",
-                                    textOverflow: "ellipsis",
-                                    overflow: "hidden"}}>{props.data.description}</p>
-                                <Button variant="success" className="card-btn" onClick={() => {window.location.href = props.data.link}}>Read More</Button>
+                            <div className="card-info">
+                                <div>
+                                    <h4>{props.data.species}</h4>
+                                    <p style={{
+                                        height: "20%",
+                                        textOverflow: "ellipsis",
+                                        overflow: "hidden"
+                                    }}>{props.data.description}</p>
+                                    <Button variant="success" className="card-btn" onClick={() => {
+                                        window.location.href = props.data.link
+                                    }}>Read More</Button>
+                                </div>
                             </div>
                         </div>
-                    </div>
                 </Modal>
                 :
-                null//sdd
+                null
             }
-                <img className="cardImage" src={props.data.image} placeholder="Image"/>
-                <div className="card-info">
-                    <div>
-                        <h4>{props.data.species}</h4>
-                        <p className="info-card-text" style={{fontSize: "16px"}}>{props.data.description}</p>
-                        <Button variant="success" className="card-btn" onClick={showAsModal}>More Info</Button>
-                    </div>
+            <img className="cardImage" src={props.data.image} placeholder="Image"/>
+            <div className="card-info">
+                <div>
+                    <h4>{props.data.species}</h4>
+                    <p className="info-card-text" style={{fontSize: "16px"}}>{props.data.description}</p>
+                    <Button variant="success" className="card-btn" onClick={toggleModal}>More Info</Button>
                 </div>
+            </div>
 
-                {/*optional edit button if a edit function was passed*/}
-                {props.onEdit != null ?
-                    <div className="controlButton">
-                        <Button variant="success" style={{marginRight: "5px"}} onClick={() => {
-                            props.onEdit(props.data)
-                        }}>Edit</Button>
-                    </div>
-                    :
-                    null
-                }
+            {/*optional edit button if a edit function was passed*/}
+            {props.onEdit != null ?
+                <div className="controlButton">
+                    <Button variant="success" style={{marginRight: "5px"}} onClick={() => {
+                        props.onEdit(props.data)
+                    }}>Edit</Button>
+                </div>
+                :
+                null
+            }
 
-                {/*optional delete button if a delete function was passed*/}
-                {props.onDelete != null ?
-                    <div className="controlButton">
-                        <Button variant="danger" onClick={() => {
-                            props.onDelete(props.data)
-                        }}>Delete</Button>
-                    </div>
-                    :
-                    null
-                }
+            {/*optional delete button if a delete function was passed*/}
+            {props.onDelete != null ?
+                <div className="controlButton">
+                    <Button variant="danger" onClick={() => {
+                        props.onDelete(props.data)
+                    }}>Delete</Button>
+                </div>
+                :
+                null
+            }
         </animated.div>
     );
 };
